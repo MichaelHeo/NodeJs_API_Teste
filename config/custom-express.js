5f96258308afd00f12adcb99d82877a39f0466b2
Michael Heo <michael.heo@hashlab.com>
2018-01-20 12:25:21 -0200

    database persistence and new post route

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var consign = require('consign')
+var bodyParser = require('body-parser')
+var expressValidator = require('express-validator')
 
 module.exports = function() {
     var app = express() // Referência do express
 
+    // Pronto para o Content-type Negotiation 
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    app.use(expressValidator())
+
     consign()
         .include('controllers')
+        .then('persistencia')
         .into(app) // Pasta controllers é definido dentro de app, o que nos permite usar a var app dentro das funções de modules.exports
 
     return app
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,7 +1,45 @@
 module.exports = function(app) {
     app.get('/pagamentos', function(req, res){
         console.log('Recebida requisição de teste')
-        res.send('OK')
+        res.send('Ok. ')
+    })
+
+    app.post('/pagamentos/pagamento', function(req, res) {
+
+        // Verificacao dos erros 
+        req.assert("forma_de_pagamento", "Forma de pagamento eh obrigatorio").notEmpty()
+        req.assert("valor", "Valor eh obrigatorio e deve ser decimal").notEmpty().isFloat()
+        // Validacao dos erros
+        var erros = req.validationErrors()
+
+        if(erros) {
+            console.log('Erros de validacao encontrados')
+            res.status(500).send(erros)
+            return
+        }
+
+        var pagamento = req.body
+        console.log('Processando uma requisicao de um novo pagamento.')
+
+        pagamento.status = 'Criado'
+        pagamento.data = new Date
+
+        var connection = app.persistencia.connectionFactory()
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection)
+        console.log(pagamentoDao)
+
+        pagamentoDao.salva(pagamento, function(erro, resultado) {
+            if(erro){
+                console.log('Erro ao inserir no bd: ' + erro)
+                res.status(400).send(erro)
+            } else {
+                console.log('pagamento criado')
+                res.location('/pagamentos/pagamento/' + resultado.insertId) // Retorna localizacao do pagamento gerado quando um recurso eh criado
+                res.status(201).json(pagamento) // 201 significa created
+                // Faz sentido ser 201, pois estamos criando uma nova instancia no bd
+            }
+        })
+
     })
 }
 
